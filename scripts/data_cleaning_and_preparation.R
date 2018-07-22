8e4cdcfeb8030691a52624d31bcafd95f6079b24
Lucho <luisalejandrolee@gmail.com>
2018-07-22 11:38:05 -0500

    Cleans data sets separately (after importing all months in a single list). Due to memory limitation I couldn't get all data in a single data.table, then treat them individually from a list.
    Allows deleting columns and transforming dates into proper format

--- scripts/data_cleaning_and_preparation.R
+++ scripts/data_cleaning_and_preparation.R
@@ -2,6 +2,8 @@
 #' Imports all chosen files
 #' @param folder Local folder with data
 #' @param files Vector with all files to import without extension (e.g. no .csv)
+#' @usage If importing several data tables with same structure (e.g. several months
+#' of the same data), could easily join aferwards with \code{rbindlist}
 #' @return List contaning imported files
 import_as_list <- function(folder, files){
   
@@ -21,4 +23,39 @@ import_as_list <- function(folder, files){
   
   return(all_files)
   
-}
+} # end of import_as_list
+
+
+#' Converts "YYYYMM" string into date format. Assumes all strings have same format
+convert_to_yearmon <- function(s){
+  
+  years <- substr(s, 1, 4)
+  months <- substr(s, 5, 6)
+  date <- paste0(years, "_",months)
+  date <- as.yearmon(date, "%Y_%m")
+  
+  return(date)
+} # end of convert_to_yearmon
+
+
+#' Transforms the list with all months of data, deleting columns, converting date
+#' into proper format and renaming variables
+#' @param dt_list A list with several data.tables, all in the same format
+#' @param cols_to_del Vector with column names to delete
+clean_dt_in_list <- function(dt_list, cols_to_del){
+  for(i in 1:length(dt_list)){
+    
+    all_months[[i]][, (cols_to_del) := NULL]
+    all_months[[i]][, PERIODO := convert_to_yearmon(PERIODO)]
+    
+  } # for
+  
+} # end of clean_dt_in_list
+
+
+
+
+
+
+
+
