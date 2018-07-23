ba0cfd0297485f919a986e897e0418bdd74376a3
Lucho <luisalejandrolee@gmail.com>
2018-07-22 22:10:21 -0500

    Since data is too large to handle in a single data.table, sampling of each month is done, so that data can be merged in a single data.table afterwards

--- scripts/data_cleaning_and_preparation.R
+++ scripts/data_cleaning_and_preparation.R
@@ -5,6 +5,7 @@
 #' @usage If importing several data tables with same structure (e.g. several months
 #' of the same data), could easily join aferwards with \code{rbindlist}
 #' @return List contaning imported files
+
 import_as_list <- function(folder, files){
   
   # initialize empty list to keep all files separated
@@ -27,6 +28,7 @@ import_as_list <- function(folder, files){
 
 
 #' Converts "YYYYMM" string into date format. Assumes all strings have same format
+
 convert_to_yearmon <- function(s){
   
   years <- substr(s, 1, 4)
@@ -43,21 +45,27 @@ convert_to_yearmon <- function(s){
 #' @param dt_list A list with several data.tables, all in the same format
 #' @param cols_to_del Vector with column names to delete
 #' @usage Changes all data.tables in the list dinamycally
+
 clean_dt_in_list <- function(dt_list, cols_to_del){
   for(i in 1:length(dt_list)){
     
-    all_months[[i]][, (cols_to_del) := NULL]
-    all_months[[i]][, PERIODO := convert_to_yearmon(PERIODO)]
-    old_cols <- names(all_months[[i]])
+    all_months[[i]][, num_id := as.character(num_id)] # convert to character
+    all_months[[i]][, (cols_to_del) := NULL] # delete columns
+    all_months[[i]][, PERIODO := convert_to_yearmon(PERIODO)] #convert to date
+    
+    # format column names (using custom function)
+    old_cols <- names(all_months[[i]]) 
     setnames(all_months[[i]], old_cols, format_columns(old_cols))
     
   } # for loop
   
 } # end of clean_dt_in_list
 
-#' Used to format column names quickly
+#' Used to format column names quickly (to lowercase and replace spaces with
+#' underscore)
 #' @param old_cols Vector of strings to modify
-#' @return Vector of formatted strings 
+#' @return Vector of formatted strings
+
 format_columns <- function(old_cols){
   
   new_cols <- sapply(old_cols, tolower) # to lower case
@@ -65,9 +73,48 @@ format_columns <- function(old_cols){
   
   return(new_cols)
   
-}
+} # end of format_columns
 
 
+#' Gets the data.tables in a list and samples them. This is used to work with
+#' samples when there's a lot of data (so that one can merge after sampling). It
+#' chooses randomly a number of clients from the first month, and chooses only 
+#' those clients for all following months (each data.table in the list)
+#' @param dt_list List with data from all months
+#' @param sample_size Number of rows (clients, assuming they are unique) to
+#' sample from the first month (first position in the list), if \code{frac == FALSE}.
+#' If \code{frac == TRUE}, must be between zero and one
+#' @param frac Whether sampling is by fraction or by number of rows
+#' @value List of the same lenght of \code{dt_list}, each element containing a 
+#' sample of the data in the original
 
+get_samples_dts <- function(dt_list, sample_size, frac = FALSE){
+  
+  # Sampling is done based on the first month
+  first_dt <- dt_list[[1]]
+  
+  # Get a sample of clients
+  if(frac == TRUE){
+    chosen_clients <- sample_frac(first_dt, sample_size)[, .(num_id)]
+  } else{
+    chosen_clients <- sample_n(first_dt, sample_size)[, .(num_id)]
+  }
+  
+  # Will hold the final data
+  sampled_dts <- vector("list", length(files)) # empty list
+  
+  i <- 1 # index
+  
+  for(i in 1:length(dt_list)){ # all datasets in the list
+    
+    temp_dt <- all_months[[i]] # current month (position in list)
+    sampled_dts[[i]] <- merge(temp_dt, chosen_clients) # get sample
+    
+    i <- i + 1 # index
+    
+  } # for loop
+  return(sampled_dts)
+  
+} # end of sample_data
 
 
